228316c98e43749d35ac4d870bc2c3321a97f3c2
0Gaine0 <jeffreygaineswrk@gmail.com>
2024-12-08 13:54:09 -0500

    Implemented functionality to load kernel
    Also implemented fat12 C implementation

--- src/kernel/boot.asm
+++ src/kernel/boot.asm
@@ -1,58 +1,45 @@
-org 0x7C00
+org 0x0
 bits 16
+
 
 %define ENDL 0x0D, 0x0A
 
+
 start:
-    jmp main
+    ; print hello world message
+    mov si, msg_hello
+    call puts
 
+.halt:
+    cli
+    hlt
 
-; prints a string to the screen.
-; params:
-;   ds:si points to string
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
 puts:
-    ; save registers
+    ; save registers we will modify
     push si
     push ax
+    push bx
 
 .loop:
-    lodsb           ; loads next character in al
-    or al, al       ; verify if next character is null
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
     jz .done
 
-    mov ah, 0x0E    ; calls bios interrupt
-    mov bh, 0
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
     int 0x10
 
     jmp .loop
 
 .done:
+    pop bx
     pop ax
-    pop si
+    pop si    
     ret
 
-main:
-
-    ; setup data segments
-    mov ax, 0
-    mov ds, ax
-    mov es, ax
-    
-    ; setup stack
-    mov ss, ax      ; setting stack to zero
-    mov sp, 0x7C00  ; stack grows downward
-    
-    ; print message
-    mov si, msg_carlyxOS
-    call puts
-
-    hlt
-
-.halt:
-    jmp .halt
-
-
-msg_carlyxOS:  db '--- Carlyx OS ---', ENDL, 0
-
-times 510 - ($ - $$) db 0
-dw 0AA55h
+msg_hello: db 'Hello world from KERNEL!', ENDL, 0
